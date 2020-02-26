e3369ec760d81bf57835ec2948cb15772797a670
AlSkylark <alxc119@gmail.com>
2020-02-26 17:35:39 +0000

    ughhhh

--- TwitterGenerator/MainWindow.xaml.cs
+++ TwitterGenerator/MainWindow.xaml.cs
@@ -13,6 +13,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 using Excel = Microsoft.Office.Interop.Excel;
+using Microsoft.Win32;
 using Tweetinvi;
 
 namespace TwitterGenerator
@@ -30,7 +31,37 @@ namespace TwitterGenerator
         private void BtnGenerate_Click(object sender, RoutedEventArgs e)
         {
             //SAVE AS FIRST
+            SaveFileDialog saveAs = new SaveFileDialog();
+            saveAs.Filter = "Excel files(*.xls or.xlsx)|.xls; *.xlsx";
+            saveAs.OverwritePrompt = true;
+            saveAs.Title = "Where do you want to save your spreadsheet?";
 
+            if (saveAs.ShowDialog() == true)
+            {
+                string location = saveAs.FileName;
+
+                //Auth.SetUserCredentials("7HvRXvXPf9pt4EaEXaKUf24Cc",
+                //                        "IYgU2IZa74bBEatM4Q41dNQN0RXVWetyFrJZlPWBxEUfx8RR7e",
+                //                        "781216822116937728-bW9jR4ZnMkZBV1qIkS8GWgkLkQIrX8L",
+                //                        "DK1QD9CsIJuvgkccw4lefVrA7Jb4SmWjvbOzZ7QDh68uR");
+
+                //var handle = User.GetUserFromScreenName("dailyskyfox");
+                //var followers = User.GetFollowers(handle, 10);
+
+                Excel.Application xlApp = new Excel.Application();
+                Excel.Workbook wbResult = new Excel.Workbook();
+                Excel.Range wbRange;
+                xlApp.Visible = true;
+                wbResult = xlApp.Workbooks.Add();
+                wbResult.Application.Visible = true;
+                wbRange = wbResult.Application.Range(0, 0);
+
+
+
+            } 
+
+
+            
         }
     }
 }
